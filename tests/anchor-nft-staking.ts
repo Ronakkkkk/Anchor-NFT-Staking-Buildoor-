11f548c2cf4cb70ae489de21848c7dd934b46af7
Ronak <ronak01.raj@gmail.com>
2024-01-13 15:56:14 +0545

    setting up nft

--- tests/anchor-nft-staking.ts
+++ tests/anchor-nft-staking.ts
@@ -1,12 +1,28 @@
-import * as anchor from "@coral-xyz/anchor";
+ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorNftStaking } from "../target/types/anchor_nft_staking";
+import { setupNft } from "./utils/setupNft";
 
 describe("anchor-nft-staking", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+
+  const wallet = anchor.workspace.AnchorNftStaking.provider.wallet
 
   const program = anchor.workspace.AnchorNftStaking as Program<AnchorNftStaking>;
+
+  let delegatedAuthPda: anchor.web3.PublicKey
+  let stakeStatePda: anchor.web3.PublicKey
+  let nft: any
+  let mintAuth: anchor.web3.PublicKey
+  let mint: anchor.web3.PublicKey
+  let tokenAddress: anchor.web3.PublicKey
+
+  before(async() => {
+    ;({nft, delegatedAuthPda, stakeStatePda, mint, mintAuth, tokenAddress} = await setupNft(program, wallet.payer))
+  })
+
 
   it("Is initialized!", async () => {
     // Add your test here.
