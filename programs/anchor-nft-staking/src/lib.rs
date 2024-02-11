7dd74f42b3639b3879b0443615e405ff2a004c31
Ronak <ronak01.raj@gmail.com>
2024-02-12 00:19:17 +0545

    unstake

--- programs/anchor-nft-staking/src/lib.rs
+++ programs/anchor-nft-staking/src/lib.rs
@@ -69,7 +69,7 @@ pub mod anchor_nft_staking {
         Ok(())
     }
 
-    pub fn redeem(ctx: Context<Stake>) -> Result<()>{
+    pub fn redeem(ctx: Context<Redeem>) -> Result<()>{
 
         require!(
             ctx.accounts.stake_state.is_initialized,
@@ -88,11 +88,11 @@ pub mod anchor_nft_staking {
             ctx.accounts.stake_state.last_stake_redeem
         );
 
-        msg!("Current time: {;?}", clock.unix_timestamp);
+        msg!("Current time: {:?}", clock.unix_timestamp);
         let unix_time = clock.unix_timestamp - ctx.accounts.stake_state.last_stake_redeem;
         msg!("Secongs since last staked: {:?}", unix_time);
         let redeem_amount = (10 * i64::pow(10,2) * unix_time) / (24*60*60);
-        msg!("Eligible reddem amount: {:?}", reddem_amount);
+        msg!("Eligible reddem amount: {:?}", redeem_amount);
 
         msg!("Minting staking rewards");
         token::mint_to(
@@ -100,7 +100,7 @@ pub mod anchor_nft_staking {
                 ctx.accounts.token_program.to_account_info(),
                 MintTo{
                     mint: ctx.accounts.stake_mint.to_account_info(),
-                    to: ctx.accounts.user_stake_ata.to_account_info().
+                    to: ctx.accounts.user_stake_ata.to_account_info(),
                     authority: ctx.accounts.stake_authority.to_account_info(),
                 }
                 &[&[
@@ -108,13 +108,96 @@ pub mod anchor_nft_staking {
                     &[*ctx.bumps.get("stake_authority").unwrap()],
                 ]],
             ),
-            reddem_amount.try_into().unwrap(),
+            redeem_amount.try_into().unwrap(),
 
         )?;
 
         ctx.accounts.stake_state.last_stake_redeem = clock.unix_time;
-        msg!("Updates last stake redeem time: {:?}", ctx.accounts.stake_state.last_stake_redeem)
+        msg!("Updates last stake redeem time: {:?}", ctx.accounts.stake_state.last_stake_redeem);
 
+
+        Ok(())
+
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()>{
+
+        require!(
+            ctx.accounts.stake_state.is_initialized,
+            StakeError::UninitializedAccount
+        );
+
+        require!(
+            ctx.accounts.stake_state.stake_state == StakeState::Staked,
+            StakeError::InvalidStakeState
+        );
+
+    msg!("Thawing token account");
+    let authority_bump = *ctx.bumps.get("program_authority").unwrap();
+    invoke_signed(
+        &thaw_delegated_account(
+            ctx.accounts.metadata_program.key(),
+            ctx.accounts.program_authority.key(),
+            ctx.accounts.nft_token_account.key(),
+            ctx.accounts.nft_edition.key(),
+            ctx.accounts.nft_mint.key(),
+        ),
+        &[
+            ctx.accounts.program_authority.to_account_info(),
+            ctx.accounts.nft_token_account.to_account_info(),
+            ctx.accounts.nft_edition.to_account_info(),
+            ctx.accounts.nft_mint.to_account_info(),
+            ctx.accounts.metadata_program.to_account_info(),
+        ],
+        &[&[b"authority", &[authority_bump]]],
+    )?;
+
+    msg!("Revoking delegate");
+
+    let cpi_revoke_program = ctx.accounts.token_program.to_account_info();
+    let cpi_revoke_accounts = Revoke {
+        source: ctx.accounts.nft_token_account.to_account_info(),
+        authority: ctx.accounts.user.to_account_info(),
+    };
+
+    let cpi_revoke_ctx = CpiContext::new(cpi_revoke_program, cpi_revoke_accounts);
+    token::revoke(cpi_revoke_ctx)?;
+
+        let clock = Clock::get()?;
+
+        msg!(
+            "Stake last redeem: {:?}",
+            ctx.accounts.stake_state.last_stake_redeem
+        );
+
+        msg!("Current time: {:?}", clock.unix_timestamp);
+        let unix_time = clock.unix_timestamp - ctx.accounts.stake_state.last_stake_redeem;
+        msg!("Secongs since last staked: {:?}", unix_time);
+        let redeem_amount = (10 * i64::pow(10,2) * unix_time) / (24*60*60);
+        msg!("Eligible reddem amount: {:?}", redeem_amount);
+
+        msg!("Minting staking rewards");
+        token::mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo{
+                    mint: ctx.accounts.stake_mint.to_account_info(),
+                    to: ctx.accounts.user_stake_ata.to_account_info(),
+                    authority: ctx.accounts.stake_authority.to_account_info(),
+                }
+                &[&[
+                    b"mint".as_ref(),
+                    &[*ctx.bumps.get("stake_authority").unwrap()],
+                ]],
+            ),
+            redeem_amount.try_into().unwrap(),
+
+        )?;
+
+        ctx.accounts.stake_state.last_stake_redeem = clock.unix_time;
+        msg!("Updates last stake redeem time: {:?}", ctx.accounts.stake_state.last_stake_redeem);
+
+        ctx.accounts.stake_state.stake_state = StakeState::Unstaked;
 
         Ok(())
 
@@ -239,7 +322,7 @@ pub struct Unstake<'info>{
         mut,
         token::authority=user
     )]
-    pub nft_token_account: Account<'info,TokenAccount>
+    pub nft_token_account: Account<'info,TokenAccount>,
     pub nft_mint: Account<'info, Mint>,
     /// CHECK: Manual Validation 
     #[account(owner=MetadataTokenId)]
@@ -251,7 +334,7 @@ pub struct Unstake<'info>{
         constraint = *user.key == stake_state.user_pubkey,
         constraint = nft_token_account.key() == stake_state.token_account
     )]
-    pub stake_state: Account<'info, UserStakeInfo>
+    pub stake_state: Account<'info, UserStakeInfo>,
     ///CHECK: manual check
     #[account(mut, seeds=["authority".as_bytes().as_ref()], bump)]
     pub program_authority: UncheckedAccount<'info>,
@@ -284,7 +367,7 @@ pub enum StakeError {
 
     #[msg("State account is unintialized")]
     UninitializedAccount,
-`       
+      
     #[msg("Stake state is invalid")]
     InvalidStakeState,
 }
